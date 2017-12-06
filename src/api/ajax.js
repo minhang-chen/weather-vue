3571152ca4859fc9086008f5e899048fa728deed
Kevin <1041874421@qq.com>
2017-12-06 20:34:17 +0800

    [feat] 封装了ajax

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -1,3 +1,37 @@
  import axios from 'axios'
 
- const instance = new axios.create()
+ let instance =  ''
+ //拦截器
+ function createInterceptors(){
+   // 发送的
+   instance.interceptors.request.use( config => config,err => {
+
+        return Promise.reject(err)
+   })
+
+   // 返回的
+   instance.interceptors.response.use(response => response, err => {
+
+       return Promise.reject(err)
+   })
+ }
+ // 创建实例
+ function createInstance(){
+   instance = axios.create({
+
+   })
+   createInterceptors()
+ }
+
+ export default ajax(url,method='get',params={},data={}){
+   return new Promise((resolve,reject) => {
+              instance.request({
+                url,
+                method,
+                params,
+                data
+              })
+              .then(data => resolve(data))
+              .catch(err => reject(err))
+   })
+ }
